2b24edfca5b350146d6d5ca96f7b05b9d61b6885
EC2 Default User <ec2-user@ip-172-31-44-123.ap-northeast-1.compute.internal>
2023-02-15 07:54:58 +0000

    [add]comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "application#hello"
+  #↑rootはルートドメイン（例 https://example.com）にアクセスしたときに表示するページの指定。
+  #rootがなければrailsのデフォルトページを表示する。
 end
