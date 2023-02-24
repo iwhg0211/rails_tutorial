10911f0ed806eb762a9843d0b0fc4d592d86e962
EC2 Default User <ec2-user@ip-172-31-44-123.ap-northeast-1.compute.internal>
2023-02-24 10:44:00 +0000

    fix branch

--- config/boot.rb
+++ config/boot.rb
@@ -1,14 +1,4 @@
-# ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
-
-# require "bundler/setup" # Set up gems listed in the Gemfile.
-# require "bootsnap/setup" # Speed up boot time by caching expensive operations.
-
-
-# 以下ChatGPTの解答
-require 'bundler/setup' 
-require 'ipaddr'
-
-# Get local IP address and use it as the default host for the application
-local_ip = IPAddr.new(Socket.ip_address_list.detect(&:ipv4_private?).ip_address)
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
-require 'bundler/setup' # Set up gems listed in the Gemfile.
+
+require "bundler/setup" # Set up gems listed in the Gemfile.
+require "bootsnap/setup" # Speed up boot time by caching expensive operations.
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
   
-  config.web_console.allowed_ips = '127.0.0.0/127.255.255.255, ::1'
+  # config.web_console.allowed_ips = '127.0.0.0/127.255.255.255, ::1'
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
